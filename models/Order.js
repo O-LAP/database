2b99a2457793d481e6d8031ce9fe619cd0bd995e
Amit Nambiar <amit.lzkpa@gmail.com>
2018-07-28 17:45:39 -0400

    schema for orders

---
+++ models/Order.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose')
+
+
+
+
+let OrderSchema = new mongoose.Schema(
+    {
+        owner: { type: String, required: true, trim: true },
+        delivery_address: { type: String, required: true, trim: true },
+        contact: { type: String, required: true },
+        message: { type: String }, 
+        created: { type: Date, default: Date.now },
+        data: Buffer,
+    },
+    {
+        timestamps: true
+    }
+)
+
+
+
+
+
+module.exports = mongoose.model('Order', OrderSchema)
