47036e9b497ce0e687730cdfb08b2450b1cc0c63
Amit Nambiar <amit.lzkpa@gmail.com>
2018-07-28 17:44:01 -0400

    cleanup

--- models/User.js
+++ models/User.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose')
 
 
 
-// https://codeburst.io/build-simple-medium-com-on-node-js-and-react-js-a278c5192f47
+// ref: https://codeburst.io/build-simple-medium-com-on-node-js-and-react-js-a278c5192f47
 let UserSchema = new mongoose.Schema(
     {
         first_name: { type: String, trim: true, required: true },
@@ -19,4 +19,16 @@ let UserSchema = new mongoose.Schema(
         timestamps: true
     }
 )
+
+
+
+
+
+
+
+
+
+
+
+
 module.exports = mongoose.model('User', UserSchema)
